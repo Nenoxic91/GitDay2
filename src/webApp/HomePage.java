837f3fff4a83d5c67ec3923a9264644015d73176
Nenoxic91 <benjamin.r.melville@gmail.com>
2023-05-30 15:08:46 -0700

    homepage warmup

---
+++ src/webApp/HomePage.java
@@ -0,0 +1,10 @@
+package webApp;
+
+public class HomePage {
+    public static void main(String[] args) {
+        System.out.println("Ben is coool");
+    }
+    //setting up homepage
+
+    //navigation menu
+}
